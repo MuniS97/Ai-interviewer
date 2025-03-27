992d2da50d8d9af2377e7eb76e4818759afdc710
MuniS97 <tursunovmunis141@gmail.com>
2025-03-27 17:04:54 +0500

    interview generation fully done

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -113,3 +113,12 @@ export async function isAuthenticated() {
 
     return !!user;
 }
+
+export async function getInterviewByUserId(userId: string): Promise<Interview[] | null> {
+    const interviews = await db.collection("interviews").where("userId", "==", userId).orderBy("createdAt", "desc").get();
+
+    return interviews.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+    })) as Interview[]
+}
